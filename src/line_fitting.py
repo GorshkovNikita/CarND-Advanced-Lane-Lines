Fixed radius computation. Added offset computation

src/line_fitting.py
@@ -9,22 +9,33 @@ mx = 3.7 / 700  # meters per pixel in x dimension
 
 
 def fit_lines(img_shape, left_line_pixel_indices, right_line_pixel_indices):
+    l_fit_coef = np.polyfit(left_line_pixel_indices[0], left_line_pixel_indices[1], 2)
+    r_fit_coef = np.polyfit(right_line_pixel_indices[0], right_line_pixel_indices[1], 2)
+    print((l_fit_coef, r_fit_coef))
+    y = np.linspace(0, img_shape[0] - 1, img_shape[0])
+
+    l_fit_x = l_fit_coef[0] * y ** 2 + l_fit_coef[1] * y + l_fit_coef[2]
+    r_fit_x = r_fit_coef[0] * y ** 2 + r_fit_coef[1] * y + r_fit_coef[2]
+
+    return l_fit_x, r_fit_x, y, l_fit_coef, r_fit_coef
+
+
+def curve_radius(left_line_pixel_indices, right_line_pixel_indices):
+    y_val = np.max(left_line_pixel_indices[0])
     l_fit = np.polyfit(left_line_pixel_indices[0] * my, left_line_pixel_indices[1] * mx, 2)
     r_fit = np.polyfit(right_line_pixel_indices[0] * my, right_line_pixel_indices[1] * mx, 2)
-    y = np.linspace(0, img_shape[0] - 1, img_shape[0]) * my
-
-    l_fit_x = l_fit[0] * y ** 2 + l_fit[1] * y + l_fit[2]
-    r_fit_x = r_fit[0] * y ** 2 + r_fit[1] * y + r_fit[2]
-
-    # in order to display these lines on source image i need to unwarp it
-    return l_fit_x / mx, r_fit_x / mx, y, l_fit, r_fit
-
-
-def curve_radius(l_fit, r_fit, y):
-    y_val = np.max(y)
-    left_curve_radius = ((1 + (2 * l_fit[0] * y_val + l_fit[1]) ** 2) ** (3 / 2)) / np.absolute(2 * l_fit[0])
-    right_curve_radius = ((1 + (2 * r_fit[0] * y_val + r_fit[1]) ** 2) ** (3 / 2)) / np.absolute(2 * r_fit[0])
+    left_curve_radius = ((1 + (2 * l_fit[0] * y_val * my + l_fit[1]) ** 2) ** (3 / 2)) / np.absolute(2 * l_fit[0])
+    right_curve_radius = ((1 + (2 * r_fit[0] * y_val * my + r_fit[1]) ** 2) ** (3 / 2)) / np.absolute(2 * r_fit[0])
     return (left_curve_radius + right_curve_radius) / 2
+
+
+def offset_from_center(img_shape, l_fit, r_fit):
+    # take x position of lines on image on the edge
+    left_x = l_fit[0] * (img_shape[0] - 1) ** 2 + l_fit[1] * (img_shape[0] - 1) + l_fit[2]
+    right_x = r_fit[0] * (img_shape[0] - 1) ** 2 + r_fit[1] * (img_shape[0] - 1) + r_fit[2]
+    ideal_car_center = np.abs(left_x) + ((img_shape[1] - (np.abs(right_x) - np.abs(left_x))) / 2)
+    real_car_center = (img_shape[1] / 2)
+    return (real_car_center - ideal_car_center) * mx
 
 
 def plot_lines(src_img, l_fit_x, r_fit_x, y):
@@ -37,11 +48,13 @@ def plot_lines(src_img, l_fit_x, r_fit_x, y):
     left_indices = np.where(left_x < src_img.shape[1])[0]
     left_filtered_x = left_x[left_indices]
 
-    left_pts = np.dstack((left_filtered_x, np.array(y / my).astype(int)[left_indices]))[0]
-    right_pts = np.dstack((right_filtered_x, np.array(y / my).astype(int)[right_indices]))[0]
+    left_pts = np.dstack((left_filtered_x, np.array(y).astype(int)[left_indices]))[0]
+    right_pts = np.dstack((right_filtered_x, np.array(y).astype(int)[right_indices]))[0]
 
     image_with_curves = np.zeros_like(src_img)
     cv2.polylines(image_with_curves, np.array([left_pts, right_pts]), False, [255, 0, 0], thickness=25)
+    # todo: draw green area
+    # cv2.fillPoly(image_with_curves, np.array([left_pts, right_pts]), [0, 255, 0])
     warped = cv2.warpPerspective(image_with_curves, m_inv, (src_img.shape[1], src_img.shape[0]))
     return cv2.addWeighted(src_img, 1.0, warped, 0.7, 1.0)
 
@@ -53,10 +66,14 @@ if __name__ == '__main__':
     image = mpimg.imread('./../out_binary_images/' + filename + '.jpg')
     f = open('./../output_lines_pixels/' + filename, 'rb')
     left_pixel_indices, right_pixel_indices = pickle.load(f)
-    left_fit_x, right_fit_x, ploty, left_fit, right_fit = \
+    left_fit_x, right_fit_x, ploty, left_fit_coef, right_fit_coef = \
         fit_lines(image.shape, left_pixel_indices, right_pixel_indices)
-    print(curve_radius(left_fit, right_fit, ploty))
+    print(curve_radius(left_pixel_indices, right_pixel_indices))
+    print(offset_from_center(image.shape, left_fit_coef, right_fit_coef))
 
     source_image = mpimg.imread('./../test_images/' + filename + '.jpg')
     plt.imshow(plot_lines(source_image, left_fit_x, right_fit_x, ploty))
+    # plt.plot(left_fit_x, ploty, color='red')
+    # plt.plot(right_fit_x, ploty, color='red')
+    # plt.imshow(image, cmap='gray')
     plt.show()

src/thresholding.py
@@ -4,7 +4,7 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 
-def sobel_binary(image, thresh=(40, 100)):
+def sobel_binary(image, thresh=(20, 300)):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
     abs_sobelx = np.absolute(sobelx)  # Absolute x derivative to accentuate lines away from horizontal
@@ -14,7 +14,7 @@ def sobel_binary(image, thresh=(40, 100)):
     return binary
 
 
-def hls_binary(image, thresh=(150, 255), axis=2):
+def hls_binary(image, thresh=(120, 255), axis=2):
     hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
     channel = hls[:, :, axis]
     binary = np.zeros_like(channel, dtype=float)
@@ -31,11 +31,12 @@ def thresholded_binary_image(image):
 
 
 if __name__ == '__main__':
-    img = mpimg.imread('./../test_images/test3.jpg')
+    img = mpimg.imread('./../test_images/test4.jpg')
     # s_binary = hls_binary(img)
     # sobel_binary = sobel_binary(img)
     # stacked_img = np.uint8(np.dstack((np.zeros_like(s_binary), s_binary, sobel_binary)) * 255)
-    # plt.imshow(stacked_img)
+    # plt.imshow(sobel_binary(img), cmap='gray')
+    # plt.imshow(hls_binary(img, axis=1), cmap='gray')
     plt.imshow(thresholded_binary_image(img), cmap='gray')
     # plt.imshow(img)
     plt.show()

src/warper.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
+# https://www.image-map.net/
 def rotation_matrix(inv=False):
     # it is possible to cache it for efficiency
     src = np.float32([[210, 718], [601, 448], [680, 446], [1106, 718]])
@@ -15,17 +16,27 @@ def rotation_matrix(inv=False):
 
 
 def warp(image, m):
-    # https://www.image-map.net/
-    # return np.rint(cv2.warpPerspective(image, m, (image.shape[1], image.shape[0]))).astype(int)
     return cv2.warpPerspective(image, m, (image.shape[1], image.shape[0]))
+
+
+def warp_and_sharpen(image, m):
+    # sharpening image after warping
+    return np.rint(warp(image, m)).astype(int)
 
 
 if __name__ == '__main__':
     from thresholding import thresholded_binary_image
+    src = np.float32([[210, 718], [601, 448], [680, 446], [1106, 718]])
+    dst = np.float32([[250, 720], [250, 0], [1000, 0], [1000, 720]])
     img = mpimg.imread('./../test_images/straight_lines1.jpg')
     # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # thresholded_image = thresholded_binary_image(img)
     m = rotation_matrix()
+    # warped_img = warp_and_convert_to_binary(thresholded_binary_image(img), m)
     warped_img = warp(img, m)
+    cv2.polylines(img, np.int32([src]), True, [255, 0, 0], thickness=3)
+    plt.imshow(img, cmap='gray')
+    plt.show()
+    cv2.polylines(warped_img, np.int32([dst]), True, [255, 0, 0], thickness=3)
     plt.imshow(warped_img, cmap='gray')
     plt.show()